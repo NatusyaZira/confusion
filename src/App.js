09d92bd545ed96842067b53628124507c0414a8b
NatusyaZira <natusya.zira@gmail.com>
2022-07-04 19:27:35 +0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
-import { Navbar } from 'reactstrap';
-
 
 
 function App() {
   return (
     <div className="App">
-    <Navbar bg="primary" variant="dark">
-    </Navbar>
+    <Container>
+      <Navbar expand="lg" variant="primary" bg="dark">
+        <Container>
+          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </Container>
     </div>
   );
 }
