019ba99e4fdb25deef67dd578885052e546e78b8
NatusyaZira <natusya.zira@gmail.com>
2022-07-06 18:38:48 +0300

    tenth commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import Dishdetail from './DishdetailComponent';
+import { DISHES } from './shared/dishes';
+import './App.css';
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+      <Navbar color="primary" dark>
+      <div className="container">
+      <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
+      </div>
+      </Navbar>
+      <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
+
+export default App;
