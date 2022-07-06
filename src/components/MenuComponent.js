e321d838e23a7bf299a49d9a8eb9d8d585c4fda9
NatusyaZira <natusya.zira@gmail.com>
2022-07-06 17:26:51 +0300

    ninth commit

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,39 +1,45 @@
+
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
+   constructor(props) {
+      super(props);
 
-    }
-  }
+      this.state = {
+         selectedDish: null
+      }
+   }
 
-  render() {
+   onDishSelect(dish) {
+      this.setState({ selectedDish: dish});
+   }
 
-    const menu = this.props.dishes.map((dish) => {
+   render() {
+      const menu = this.props.dishes.map((dish) => {
+         return (
+            <div className="col-12 col-md-5 m-1">
+               <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+                  <CardImg width="100%" src={dish.image} alt={dish.name} />
+                  <CardImgOverlay>
+                     <CardTitle>{dish.name}</CardTitle>
+                  </CardImgOverlay>
+               </Card>
+            </div>
+         );
+      });
+
       return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-    <Card tag="ul" className="row">
-
-        <CardImg width="100%" src={dish.image} alt={dish.name} />
-      <CardImgOverlay>
-        <CardTitle>{dish.name}</CardTitle>
-      </CardImgOverlay>
-    </Card>
-  </div>
+         <div className="container">
+            <div className="row">
+               {menu}
+            </div>
+            <Dishdetail dish={this.state.selectedDish} />
+         </div>
       );
-    });
-
-    return (
-      <div className="container">
-      <div className="row">
-          {menu}
-      </div>
-      </div>
-    );
-  }
+   }
 }
 
 export default Menu;
